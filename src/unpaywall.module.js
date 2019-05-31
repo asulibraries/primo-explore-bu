590eff491c4e52fd8cab940969f04458e16971f7
Eli Zoller <zoller.eli@gmail.com>
2019-05-31 10:17:36 -0700

    swap custom icon with primo icon

--- src/unpaywall.module.js
+++ src/unpaywall.module.js
@@ -118,7 +118,7 @@ angular.module('bulibUnpaywall', [])
         <div layout="flex" ng-if="$ctrl.best_oa_link" class="layout-row" style="margin-top: 5px;">\
           <a ng-click="$ctrl.trackLinkClick($ctrl.doi)" target="_blank" href="{{$ctrl.best_oa_link}}"\
             style="margin-left: 3px; margin-top: 3px;" rel="noreferrer">\
-            <prm-icon icon-type="svg" svg-icon-set="action" icon-definition="ic_lock_open_24px" style="color: #f68212;"></prm-icon>\
+            <prm-icon icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-access" class="open-access-mark-icon"></prm-icon>\
             View Open Access Version\
             <span ng-if="$ctrl.showVersionLabel && $ctrl.best_oa_version">&nbsp({{$ctrl.best_oa_version}} version)</span>\
             <prm-icon external-link icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new"></prm-icon>\
